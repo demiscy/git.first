81a45bf787e39f61b29dab27486dbdda3b36a1ab
demiscy <dtrihinas@hotmail.com>
2013-06-15 16:35:16 +0300

    new class Testing edited GitTest

--- demiscy.git.first/src/demiscy/git/first/GitTest.java
+++ demiscy.git.first/src/demiscy/git/first/GitTest.java
@@ -2,6 +2,7 @@ package demiscy.git.first;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Git is cool");
+		System.out.println("Git is cool!");
+		System.out.println("Git and i like it!");
 	}
 } 
---
+++ demiscy.git.first/src/demiscy/git/first/Testing.java
@@ -0,0 +1,13 @@
+package demiscy.git.first;
+
+public class Testing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("just testing");
+
+	}
+
+}
